0867ac6e24e391954af7230d7acffee16dce8f20
Jonathan Moya <djgrovezone@gmail.com>
2025-04-23 11:59:53 +0200

    feat(auth): add login action for server-side authentication
    This commit introduces a new server-side login action to handle authentication using credentials. The action includes error handling for invalid credentials and other potential authentication errors. The `login` function is also added to simplify the login process with email and password inputs.

---
+++ src/actions/auth/login.ts
@@ -0,0 +1,52 @@
+'use server';
+ 
+import { signIn } from '@/auth.config';
+import { AuthError } from 'next-auth';
+ 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    // await signIn('credentials', formData);
+    //? Podemos manejar el return manualmente y mandar redirect a false si queremos así:
+    await signIn('credentials', {
+      ...Object.fromEntries(formData), 
+      redirect: false
+    }); 
+    
+    return 'Success';
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
+
+export const login = async (email: string, password: string) => {
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirect: false
+    }); 
+
+    return {
+      ok: true,
+    }
+  } catch (error) {
+    console.error(error); 
+
+    return {
+      ok: false,
+      message: 'No se pudo iniciar sesión'
+    }
+  } 
+}
