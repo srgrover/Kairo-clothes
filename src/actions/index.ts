9740e8daf8278e558e43aef4f5fe90b8a8b8bc1e
Jonathan Moya <djgrovezone@gmail.com>
2025-04-23 12:33:21 +0200

    feat: add index exports for actions, store, and utils modules

---
+++ src/actions/index.ts
@@ -0,0 +1,28 @@
+export * from './product/product-pagination';
+export * from './product/get-product-by-slug';
+export * from './product/get-stock-by-slug';
+export * from './product/create-update-product';
+export * from './product/delete-product-image';
+
+export * from './auth/login';
+export * from './auth/logout';
+export * from './auth/register';
+
+export * from './country/getCountries';
+
+export * from './address/set-user-address';
+export * from './address/delete-user-address';
+export * from './address/get-user-address';
+
+export * from './order/place-order';
+export * from './order/get-order-by-id';
+export * from './order/get-orders-by-user';
+export * from './order/get-paginated-orders';
+
+export * from './payments/set-transaction-id';
+export * from './payments/paypal-check-payment';
+
+export * from './users/get-paginated-users';
+export * from './users/change-user-role';
+
+export * from './categories/get-categories';
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './generationPaginationNumbers'
+export * from './currencyFormat'
