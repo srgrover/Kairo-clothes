7ffd320184f96dcce2fddc4c94538036bf44e9db
Jonathan Moya <djgrovezone@gmail.com>
2025-04-23 19:50:29 +0200

    Refactor error handling in authenticate function to simplify authentication error messages

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -2,7 +2,7 @@
  
 import { signIn } from '@/auth.config';
 import { AuthError } from 'next-auth';
- 
+
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
@@ -18,12 +18,7 @@ export async function authenticate(
     return 'Success';
   } catch (error) {
     if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
+      return 'Authentication error.';
     }
     throw error;
   }
