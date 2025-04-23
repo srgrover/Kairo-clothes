b4c0f3d8e240eea072cba615393b6a8ff381a5af
Jonathan Moya <djgrovezone@gmail.com>
2025-04-23 11:37:27 +0200

    feat(database): add initial database schema and migrations
    This commit introduces the initial database schema and migrations for the application. It includes tables for users, products, categories, orders, and related entities, along with necessary relationships and constraints. The schema is designed to support core functionalities such as user management, product catalog, and order processing.

---
+++ prisma/migrations/20250324114803_user_address/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "UserAddress" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "address2" TEXT,
+    "city" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "countryId" TEXT,
+    "userId" TEXT,
+
+    CONSTRAINT "UserAddress_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserAddress_userId_key" ON "UserAddress"("userId");
+
+-- AddForeignKey
+ALTER TABLE "UserAddress" ADD CONSTRAINT "UserAddress_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserAddress" ADD CONSTRAINT "UserAddress_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
